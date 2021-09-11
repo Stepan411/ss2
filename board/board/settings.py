af85223a701ef57b03631d96e9695998d92442b4
Stepan411 <ss.vizor@gmail.com>
2021-09-11 19:58:43 +0300

    11_09_1

--- board/board/settings.py
+++ board/board/settings.py
@@ -74,6 +74,7 @@ WSGI_APPLICATION = 'board.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
